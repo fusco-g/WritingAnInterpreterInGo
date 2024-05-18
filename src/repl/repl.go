220fe5a99088eaae8965bfec94554201bfb8b954
elia renzoni <elia.renzoni03@gmail.com>
2024-05-18 11:30:36 +0200

    add repl.go

--- src/repl/repl.go
+++ src/repl/repl.go
@@ -1,1 +1,27 @@
 package repl
+
+import (
+	"bufio"
+	"fmt"
+	"interpeter/lexer"
+	"interpeter/token"
+	"io"
+)
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+	for {
+		fmt.Printf("Source Code: \n")
+		if scanned := scanner.Scan(); !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.NewLexer(line)
+
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+
+}
