a9630d0d562795c9ca38f5f2b4272962af721726
unknown <elia.renzoni03@gmail.com>
2024-06-18 09:39:58 +0200

    add parser.go

--- src/parser/parser.go
+++ src/parser/parser.go
@@ -1,1 +1,19 @@
 package parser
+
+import "interpeter/token"
+
+type Parser struct {
+	tokenInstance token.Token
+
+	// pointer to the current token
+	currentToken token.Token
+
+	// pointer to the next token
+	nextToken token.Token
+}
+
+func NewParser() *Parser {
+	return &Parser{
+		// TODO
+	}
+}
