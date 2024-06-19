a56e3805942ddf3201488a3b88094ecad798c3f5
unknown <elia.renzoni03@gmail.com>
2024-06-19 09:13:57 +0200

    update parser.go

--- src/parser/parser.go
+++ src/parser/parser.go
@@ -1,9 +1,13 @@
 package parser
 
-import "interpeter/token"
+import (
+	"interpeter/ast"
+	"interpeter/lexer"
+	"interpeter/token"
+)
 
 type Parser struct {
-	tokenInstance token.Token
+	lexerInstance lexer.Lexer
 
 	// pointer to the current token
 	currentToken token.Token
@@ -12,8 +16,18 @@ type Parser struct {
 	nextToken token.Token
 }
 
-func NewParser() *Parser {
-	return &Parser{
-		// TODO
-	}
+func NewParser(lexer lexer.Lexer) *Parser {
+	p := &Parser{lexerInstance: lexer}
+	p.nToken()
+	p.nToken()
+	return p
+}
+
+func (p *Parser) nToken() {
+	p.currentToken = p.nextToken
+	p.nextToken = p.lexerInstance.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
 }
